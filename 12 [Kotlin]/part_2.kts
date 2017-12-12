051015c0d05e759c9667bc1fcfd538ce7cdf93a6
Paweł Świątkowski <katafrakt@vivaldi.net>
2017-12-12 12:32:46 +0100

    day 12

---
+++ 12 [Kotlin]/part_2.kts
@@ -0,0 +1,39 @@
+import java.io.File
+
+val regex = Regex("(\\d+) \\<-\\> (.+)")
+val pipes: HashMap<Int, List<Int>> = hashMapOf()
+
+fun processLine(line: String) {
+    val match = regex.matchEntire(line)
+    val (nameStr, connectionsStr) = match!!.destructured
+    val key = nameStr.toInt()
+    val values = connectionsStr.split(", ").map { str -> str.toInt() }
+    pipes.put(key, values)
+}
+
+val lines = mutableListOf<String>()
+File("input").useLines { ls -> ls.forEach { processLine(it) }}
+
+var numOfGroups = 0;
+while (!pipes.isEmpty()) {
+    numOfGroups++
+    val sample = pipes.keys.first()
+    val toCheck = mutableListOf(sample);
+    val group = hashSetOf(sample);
+
+    while (!toCheck.isEmpty()) {
+        val elem = toCheck.first()
+        toCheck.remove(elem)
+        
+        if (pipes.containsKey(elem)) {
+            for (c in pipes.get(elem)!!.iterator()) {
+                group.add(c)
+                toCheck.add(c)
+            }
+        }
+        
+        pipes.remove(elem)
+    }
+}
+
+println(numOfGroups)
