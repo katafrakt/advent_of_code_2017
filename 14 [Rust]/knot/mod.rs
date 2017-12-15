c5e62f7173e9b08fd001d70ead876ef48b6560d8
Paweł Świątkowski <katafrakt@vivaldi.net>
2017-12-15 17:41:41 +0100

    Extract module

---
+++ 14 [Rust]/knot/mod.rs
@@ -0,0 +1,38 @@
+pub fn hash(input: &str) -> Vec<usize> {
+    let mut length_list = input.as_bytes().to_vec();
+    length_list.append(&mut vec![17, 31, 73, 47, 23]);
+    let mut current_pos = 0;
+    let mut skip_size: i32 = 0;
+    let mut index_offset: i32 = 0;
+    let mut list: Vec<usize> = (0..256).collect();
+
+    for _t in 0..64 {
+        for length_ptr in &length_list {
+            let length = *length_ptr as usize;
+            let mut sublist = list.split_off(length);
+            list.reverse();
+            list.append(&mut sublist);
+            let offset: i32 = (skip_size + length as i32) % (list.len() as i32);
+            sublist = list.split_off(offset as usize);
+            sublist.append(&mut list);
+            list = sublist;
+            skip_size += 1;
+            index_offset = (index_offset + 256 - offset) % 256;
+            current_pos = (current_pos + 1) % (list.len() as usize);
+        }
+    }
+
+    let mut sublist = list.split_off((index_offset % 256) as usize);
+    sublist.append(&mut list);
+    list = sublist;
+
+    let mut result = Vec::new();
+    for _i in 0..16 {
+        let tail = list.split_off(16);
+        let xored = list.iter().fold(0, |acc, x| acc ^ x);
+        result.push(xored);
+        list = tail;
+    };
+    
+    return result;
+}
