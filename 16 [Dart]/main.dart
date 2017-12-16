b079ca49aad2012f6c255b653d39e60b25b1dd4d
Paweł Świątkowski <katafrakt@vivaldi.net>
2017-12-16 18:43:34 +0100

    day 16, part 2

--- 16 [Dart]/main.dart
+++ 16 [Dart]/main.dart
@@ -13,10 +13,7 @@ swapElements(list, from, to) {
   list[from] = tmp;
 }
 
-main() async {
-  var list = "abcdefghijklmnop".split("");
-  var instructions = await new File('input').readAsString();
-  var steps = instructions.split(",");
+performSteps(list, steps) {
   for (var step in steps) {
     var code = step[0];
     var data = step.substring(1);
@@ -37,6 +34,34 @@ main() async {
         var to = list.indexOf(parts[1]);
         swapElements(list, from, to);
     }
+  }
+  return list;
+}
+
+main() async {
+  const billion = 1000000000;
+  const start = "abcdefghijklmnop";
+  var list = start.split("");
+  var instructions = await new File('input').readAsString();
+  var steps = instructions.split(",");
+  var period = billion;
+
+  for (var i = 0; i < billion; i++) {
+    list = performSteps(list, steps);
+
+    var configuration = list.join();
+    
+    if (i == 0) print(configuration);
+    
+    if (configuration == start) {
+      period = i + 1;
+      break;
+    }
+  }
+  
+  var times = billion % period;
+  for (var i = 0; i < times; i++) {
+    list = performSteps(list, steps);
   }
   print(list.join(""));
 }
