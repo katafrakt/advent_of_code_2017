de3444880e9f498fce7761344d324cc3324c03ac
Paweł Świątkowski <katafrakt@vivaldi.net>
2017-12-31 21:42:20 +0100

    day 23, part 2

--- 23 [Java]/part_2.java
+++ 23 [Java]/part_2.java
@@ -19,26 +19,18 @@ class Part2 {
             c += 17000;
         }
 
-        // outernmost loop
         do {
             f = 1;
             d = 2;
             do {
                 e = 2;
-                do {
-                    g = d;
-                    g *= e;
-                    g -= b;
-                    if (g == 0) f = 0;
-                    e += 1;
-                    g = e;
-                    g -= b;
-                } while (g != 0);
+                if (b % d == 0) f = 0;
+                e = b;
+                g = 0;
 
                 d += 1;
                 g = d;
                 g -= b;
-                //System.out.println(g);
             } while (g != 0);
 
             if (f == 0) h++;
@@ -47,7 +39,7 @@ class Part2 {
             g -= c;
             
             if (g == 0) break;
-            b += 17;
+            b += 17;  
         } while (true);
 
         System.out.println(h);
